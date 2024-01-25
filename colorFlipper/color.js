74eb025b5c94c541787b5566ffbb1b6e75e586ad
Raghava <moorthy46030@gmail.com>
2024-01-25 22:51:28 +0530

    Stopper Added

--- colorFlipper/color.js
+++ colorFlipper/color.js
@@ -4,8 +4,10 @@ const head1 = document.getElementById("h1");
 const color1 = document.getElementById("h2-c1");
 const color2 = document.getElementById("h2");
 const btn = document.getElementById("butn");
+const colorArr = document.getElementById("colorAr");
 let interval = null;
 const values = "0123456789abcdef";
+let colorAr = [];
 btn.onclick = toggleStartStop;
 btn.onmouseover = () => {
   btn.classList.remove("animation");
@@ -13,20 +15,23 @@ btn.onmouseover = () => {
 function run(msg) {
   btn.innerHTML = msg;
   if (msg === "stop" && interval === null) {
+    btn.classList.add("animation");
     btn.onmouseleave = () => {
       btn.classList.add("animation");
     };
     interval = setInterval(() => {
       let hex = "#";
       for (let i = 0; i < 6; i++) hex += colorFlip();
-
       let val = container.style.backgroundColor;
       color1.innerHTML = color2.innerHTML;
+      colorAr.push(color1.innerHTML);
       color2.innerHTML = hex;
       header.style.backgroundColor = val;
       head1.style.color = hex;
       color1.style.color = hex;
       color2.style.color = hex;
+      colorArr.innerHTML = colorAr.join(", ");
+      colorArr.style.color = val;
       btn.style.color = getComputedStyle(container).backgroundColor;
       container.style.backgroundColor = hex;
       btn.style.textShadow = `4px 3px 3px ${
@@ -36,6 +41,8 @@ function run(msg) {
   } else {
     clearInterval(interval);
     interval = null;
+    btn.classList.remove("animation");
+
     btn.onmouseleave = () => {
       btn.classList.remove("animation");
     };
